bda2fd07e3aa4cc116eaee2df09bf17398d81e73
Arthur <74980776+arth-inacio@users.noreply.github.com>
2025-06-01 18:33:41 -0300

    Update start.py

--- start.py
+++ start.py
@@ -7,5 +7,6 @@ if sys.platform.startswith("win"):
 
 import uvicorn
 
+#Mudar localhost para a porta desejada
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=False)
